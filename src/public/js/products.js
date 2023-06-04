f7085b9b5afefa5f21c18979f5acef85c1964d2a
Ivan Pasciucco <100731598+Ivan-Pasciucco@users.noreply.github.com>
2023-06-03 23:51:14 -0300

    agrego productos y carrito modal

---
+++ src/public/js/products.js
@@ -0,0 +1,109 @@
+const products = [
+  {
+    id: 1,
+    nombre: "Vino Malbec",
+    descripcion:
+      "Un vino tinto argentino con notas intensas de frutas maduras, taninos suaves y un final persistente.",
+    imagen: "https://ejemplo.com/vino-malbec.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Vino Cabernet Sauvignon",
+    descripcion:
+      "Un vino tinto de cuerpo completo con aromas a grosella negra, especias y taninos firmes.",
+    imagen: "https://ejemplo.com/vino-cabernet-sauvignon.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Vino Chardonnay",
+    descripcion:
+      "Un vino blanco de estilo elegante con notas de frutas tropicales, vainilla y una acidez refrescante.",
+    imagen: "https://ejemplo.com/vino-chardonnay.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Vino Merlot",
+    descripcion:
+      "Un vino tinto suave y afrutado con sabores de ciruelas, cerezas y toques de especias.",
+    imagen: "https://ejemplo.com/vino-merlot.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Vino Pinot Noir",
+    descripcion:
+      "Un vino tinto delicado con aromas a frutas rojas, flores y una estructura ligera.",
+    imagen: "https://ejemplo.com/vino-pinot-noir.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Vino Sauvignon Blanc",
+    descripcion:
+      "Un vino blanco fresco y vibrante con notas cítricas, hierbas y una acidez refrescante.",
+    imagen: "https://ejemplo.com/vino-sauvignon-blanc.jpg",
+  },
+  {
+    id: 7,
+    nombre: "Vino Syrah/Shiraz",
+    descripcion:
+      "Un vino tinto especiado y potente con sabores a moras, pimienta y notas ahumadas.",
+    imagen: "https://ejemplo.com/vino-syrah-shiraz.jpg",
+  },
+  {
+    id: 8,
+    nombre: "Vino Riesling",
+    descripcion:
+      "Un vino blanco aromático con notas de frutas blancas, flores y una acidez equilibrada.",
+    imagen: "https://ejemplo.com/vino-riesling.jpg",
+  },
+  {
+    id: 9,
+    nombre: "Vino Malbec-Cabernet Sauvignon",
+    descripcion:
+      "Un vino tinto complejo y estructurado con la combinación de las variedades Malbec y Cabernet Sauvignon.",
+    imagen: "https://ejemplo.com/vino-malbec-cabernet-sauvignon.jpg",
+  },
+  {
+    id: 10,
+    nombre: "Vino Tempranillo",
+    descripcion:
+      "Un vino tinto español con sabores a frutas maduras, especias y una textura suave en boca.",
+    imagen: "https://ejemplo.com/vino",
+  },
+  [
+    {
+      id: 11,
+      nombre: "Vino Malbec Reserva",
+      descripcion:
+        "Un vino tinto argentino de alta gama con intensos aromas a frutas maduras, madera y taninos elegantes.",
+      imagen: "https://ejemplo.com/vino-malbec-reserva.jpg",
+    },
+    {
+      id: 12,
+      nombre: "Vino Cabernet Franc",
+      descripcion:
+        "Un vino tinto elegante con aromas a frutas rojas, especias y una acidez equilibrada.",
+      imagen: "https://ejemplo.com/vino-cabernet-franc.jpg",
+    },
+    {
+      id: 13,
+      nombre: "Vino Garnacha",
+      descripcion:
+        "Un vino tinto español con notas de frutas negras, especias y un final suave y sedoso.",
+      imagen: "https://ejemplo.com/vino-garnacha.jpg",
+    },
+    {
+      id: 14,
+      nombre: "Vino Verdejo",
+      descripcion:
+        "Un vino blanco español fresco y aromático con sabores cítricos, hierbas y una acidez vibrante.",
+      imagen: "https://ejemplo.com/vino-verdejo.jpg",
+    },
+    {
+      id: 15,
+      nombre: "Vino Pinot Grigio",
+      descripcion:
+        "Un vino blanco italiano ligero y refrescante con notas de pera, melocotón y una delicada mineralidad.",
+      imagen: "https://ejemplo.com/vino-pinot-grigio.jpg",
+    },
+  ],
+];
